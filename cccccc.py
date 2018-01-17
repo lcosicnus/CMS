25190ac28640ec44600b0806ba1ca914ddbc5aee
lcosicnus <lcosicnus@gmail.com>
2018-01-17 14:36:10 +0100

    speed estimation

--- cccccc.py
+++ cccccc.py
@@ -2,16 +2,26 @@ import cv2
 import dlib
 import time
 import threading
+import math
 
 carCascade = cv2.CascadeClassifier('myhaar.xml')
-video = cv2.VideoCapture('video//prvi.mkv')
+video = cv2.VideoCapture('video//cetvrti.mkv')
 
 WIDTH = 720
 HEIGHT = 560
+carWidht = 1.85
 
 # def carNumber(carNum, cID):
 	# time.sleep(2)
 	# carNum[cID] = 'Car ' + str(cID)
+
+
+def estimateSpeed(location1, location2, mySpeed,fps):
+	d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
+	ppm = location2[2] / carWidht
+	d_meters = d_pixels / ppm
+	speed = mySpeed + d_meters * fps
+	return speed
 	
 def trackMultipleObjects():
 	rectangleColor = (0, 0, 255)
@@ -21,6 +31,8 @@ def trackMultipleObjects():
 	
 	carTracker = {}
 	carNumbers = {}
+	carLocation1 = {}
+	carLocation2 = {}
 	
 	while True:
 		start_time = time.time()
@@ -42,11 +54,15 @@ def trackMultipleObjects():
 				
 		for carID in carIDtoDelete:
 			print 'Removing carID ' + str(carID) + ' from list of trackers.'
+			print 'Removing carID ' + str(carID) + ' previous location.'
+			print 'Removing carID ' + str(carID) + ' current location.'
 			carTracker.pop(carID, None)
+			carLocation1.pop(carID, None)
+			carLocation2.pop(carID, None)
 		
 		if not (frameCounter % 10):
 			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-			cars = carCascade.detectMultiScale(gray, 1.1, 12, 0, (24, 24))
+			cars = carCascade.detectMultiScale(gray, 1.1, 13, 0, (24, 24))
 			
 			for (_x, _y, _w, _h) in cars:
 				x = int(_x)
@@ -69,6 +85,8 @@ def trackMultipleObjects():
 					
 					t_x_bar = t_x + 0.5 * t_w
 					t_y_bar = t_y + 0.5 * t_h
+					
+					
 				
 					if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
 						matchCarID = carID
@@ -80,7 +98,7 @@ def trackMultipleObjects():
 					tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
 					
 					carTracker[currentCarID] = tracker
-					
+					carLocation1[currentCarID] = [x, y, w, h]
 					# t = threading.Thread(target = carNum, args = (carNumbers, currentCarID))
 					# t.start()
 					
@@ -96,6 +114,9 @@ def trackMultipleObjects():
 			
 			cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 2)
 			
+			# speed estimation
+			carLocation2[carID] = [t_x, t_y, t_w, t_h]
+				
 			# if carID in carNumbers.keys():
 				# cv2.putText(resultImage, carNumbers[carID], (int(t_x + t_w/2), int(t_y)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 			# else:
@@ -105,6 +126,16 @@ def trackMultipleObjects():
 		if not (end_time == start_time):
 			fps = 1.0/(end_time - start_time)
 		cv2.putText(resultImage, 'FPS: ' + str(int(fps)), (620, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+		for i in carLocation1.keys():
+			if frameCounter % 10 == 0:
+				[x1, y1, w1, h1] = carLocation1[i]
+				[x2, y2, w2, h2] = carLocation2[i]
+				# print 'previous location: ' + str(carLocation1[i]) + ', current location: ' + str(carLocation2[i])
+				carLocation1[i] = [x2, y2, w2, h2]
+				# print 'new previous location: ' + str(carLocation1[i])
+				if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
+					speed = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2], 100, fps)
+					print 'CarID ' + str(i) + ' speed is ' + str("%.2f" % round(speed, 2)) + ' km/h.\n'
 		cv2.imshow('result', resultImage)
 				
 		if cv2.waitKey(33) == 27:
