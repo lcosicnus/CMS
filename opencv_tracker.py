7371ba0045fd432f3ee6e2a5f46c1e1061aa43f4
lcosicnus <lcosicnus@gmail.com>
2018-02-22 15:04:47 +0100

    speed estimation - center of ROI

--- opencv_tracker.py
+++ opencv_tracker.py
@@ -3,6 +3,7 @@ import math as m
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import statistics as st
 
 carCascade = cv2.CascadeClassifier('myhaar.xml')
 video = cv2.VideoCapture('video//prvi.mkv')
@@ -10,22 +11,37 @@ video = cv2.VideoCapture('video//prvi.mkv')
 
 def estimate_speed(bbox_p, bbox_c, seconds):
 	#~ print('p: ' + str(bbox_p))
-	#~ print('c: ' + str(bbox_c))
+	#~ print('c: ' + str(bbox_c))	
 	
 	center_p_x = bbox_p[0] + 0.5 * bbox_p[2]
 	center_p_y = bbox_p[1] + 0.5 * bbox_p[3]
-	area_p = bbox_p[2] * bbox_p[3]
+	
+	print('previous: ')
+	print('x = ' + str(bbox_p[0]))
+	print('y = ' + str(bbox_p[1]))
+	print('w = ' + str(bbox_p[2]))
+	print('h = ' + str(bbox_p[3]))
+	print(center_p_x)
+	print(center_p_y)
 
 	center_c_x = bbox_c[0] + 0.5 * bbox_c[2]
 	center_c_y = bbox_c[1] + 0.5 * bbox_c[3]
-	area_c = bbox_c[2] * bbox_c[3]
+
+	print('current: ')
+	print('x = ' + str(bbox_c[0]))
+	print('y = ' + str(bbox_c[1]))
+	print('w = ' + str(bbox_c[2]))
+	print('h = ' + str(bbox_c[3]))
+	print(center_c_x)
+	print(center_c_y)
 
 	distance_pixels = m.sqrt(m.pow(float(center_c_x - center_p_x), 2) + m.pow(float(center_c_y - center_p_y), 2))
-	ppm = bbox_c[2] / 2
+	ppm = ((bbox_c[2] + bbox_p[2]) / 2.0) / 2.0
 	distance_meters = distance_pixels / ppm
 	
 	speed = distance_meters / seconds
 	
+	print('ppm = ' + str(ppm))
 	print('t = ' + str(seconds) + ' s')
 	print('s = ' + str(distance_pixels) + ' pixel')
 	print('s = ' + str(distance_meters) + ' m')
@@ -42,6 +58,7 @@ def tracker():
 	
 	carTracker = {}
 	v = []
+	median = []
 	
 	while True:
 		rc, image = video.read()
@@ -83,9 +100,10 @@ def tracker():
 				y = int(_y) + 5
 				w = int(_w) - 5
 				h = int(_h) - 5
-			
+				
 				x_bar = x + 0.5 * w
 				y_bar = y + 0.5 * h
+				
 				
 				matchCarID = None
 			
@@ -102,16 +120,17 @@ def tracker():
 										
 					if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
 						matchCarID = carID
-				
+								
 				if matchCarID is None:
 					bbox = (x, y, w, h)
-					#tracker = cv2.Tracker_create('BOOSTING') #tracker
-					#tracker = cv2.TrackerBoosting_create() #tracker
-					tracker = cv2.TrackerMedianFlow_create() #tracker
-					tracker.init(image, bbox)
-					carTracker[currentCarID] = tracker	
-					previous_location[currentCarID] = bbox
-					currentCarID = currentCarID + 1
+					if bbox[0] < 500 or bbox[1] < 100:
+						#tracker = cv2.Tracker_create('BOOSTING') #tracker
+						#tracker = cv2.TrackerBoosting_create() #tracker
+						tracker = cv2.TrackerMedianFlow_create() #tracker
+						tracker.init(image, bbox)
+						carTracker[currentCarID] = tracker	
+						previous_location[currentCarID] = bbox
+						currentCarID = currentCarID + 1
 					
 		for carID in carTracker.keys():
 			trackedPosition = carTracker[carID].update(image)
@@ -124,17 +143,13 @@ def tracker():
 			t_x_bar = t_x + 0.5 * t_w
 			t_y_bar = t_y + 0.5 * t_h
 			bbox = (t_x, t_y, t_w, t_h)
-			
-			#-------------------------------------------------------------------------------------
-			#Corner detection
-			#-------------------------------------------------------------------------------------
-			#~ gray_ROI = gray[y:y + h, x:x + w]
-			#~ corners = cv2.goodFeaturesToTrack(gray_ROI, 25, 0.01, 10)
-			#~ if type(corners) != type(None):
-				#~ corners = np.int0(corners)
-			#~ for i in corners:
-				#~ x,y = i.ravel()
-				#~ cv2.circle(resultImage, (x, y), 3, 255, 2)
+			corners = cv2.goodFeaturesToTrack(gray, 25, 0.01, 10)
+			if type(corners) != type(None):
+				corners = np.int0(corners)
+			for i in corners:
+				x,y = i.ravel()
+				if t_x < x < t_x + t_w and t_y < y < t_y + t_h:
+					cv2.circle(resultImage, (x, y), 5, 255, -1)
 			
 			current_location[carID] = bbox
 				
@@ -146,8 +161,6 @@ def tracker():
 		fps = 1.0 / seconds
 		cv2.putText(resultImage, 'FPS: ' + 	str(int(fps)), (800, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 3)
 		
-		
-		
 		for i in previous_location.keys():
 			if frameCounter % 1 == 0:
 				bbox_p = previous_location[i]
@@ -155,16 +168,40 @@ def tracker():
 				
 				previous_location[i] = current_location[i]
 				if bbox_p != bbox_c:
-					speed = estimate_speed(bbox_p, bbox_c, seconds) 
-					v.append(speed)
+					estimate_speed(bbox_p, bbox_c, seconds)
 		
 		cv2.imshow('image', resultImage)
 		if cv2.waitKey(33) == 27:
 			break
-		
-	i = range(0, len(v))
-	plt.plot(i, v)
-	plt.show()
+			
 	cv2.destroyAllWindows()
 if __name__ == '__main__':
 	tracker()
+
+#-------------------------------------------------------------------------------------
+#Corner detection
+#-------------------------------------------------------------------------------------
+#~ gray_ROI = gray[y:y + h, x:x + w]
+#~ corners = cv2.goodFeaturesToTrack(gray_ROI, 25, 0.01, 10)
+#~ if type(corners) != type(None):
+	#~ corners = np.int0(corners)
+#~ for i in corners:
+	#~ x,y = i.ravel()
+	#~ cv2.circle(resultImage, (x, y), 3, 255, 2)
+
+#----------------------------
+# Statistics
+#----------------------------
+#~ med = []
+#~ average = []
+#~ i = range(0, len(v))
+#~ avg = np.mean(v)
+#~ m = st.median(v)
+#~ for x in i:
+	#~ average.append(avg)
+	#~ med.append(m)
+#~ plt.plot(i, v)
+#~ plt.plot(i, average)
+#~ plt.plot(i, med)
+#~ plt.legend(['speed', 'average', 'median'])
+#~ plt.show()
