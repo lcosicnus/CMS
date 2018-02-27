7b4df76e4d9ca84759cdd5d884a78761300c1160
lcosicnus <lcosicnus@gmail.com>
2018-02-27 11:38:20 +0100

    Median flow

--- opencv_tracker.py
+++ opencv_tracker.py
@@ -8,11 +8,18 @@ import statistics as st
 carCascade = cv2.CascadeClassifier('myhaar.xml')
 video = cv2.VideoCapture('video//prvi.mkv')
 
-
+#------------------------------------------------------------------------------------------
+# Speed estimation
+#------------------------------------------------------------------------------------------
 def estimate_speed(bbox_p, bbox_c, seconds):
-	#~ print('p: ' + str(bbox_p))
-	#~ print('c: ' + str(bbox_c))	
+	#~ if some of coordinates is 0, estimated speed is 0
+	for i in range(0, len(bbox_p)):
+		if bbox_p[i] == 0 or bbox_c[i] == 0:
+			return 0.000000000000001
 	
+	#~ calculate center point of previous bounding box
+	#~ print x and y coordinates of top left point of bounding box
+	#~ print width and height of bounding box and calculated center point of bounding box
 	center_p_x = bbox_p[0] + 0.5 * bbox_p[2]
 	center_p_y = bbox_p[1] + 0.5 * bbox_p[3]
 	
@@ -24,6 +31,9 @@ def estimate_speed(bbox_p, bbox_c, seconds):
 	print(center_p_x)
 	print(center_p_y)
 
+	#~ calculate center point of current bounding box
+	#~ print x and y coordinates of top left point of bounding box
+	#~ print width and height of bounding box and calculated center point of bounding box
 	center_c_x = bbox_c[0] + 0.5 * bbox_c[2]
 	center_c_y = bbox_c[1] + 0.5 * bbox_c[3]
 
@@ -34,13 +44,19 @@ def estimate_speed(bbox_p, bbox_c, seconds):
 	print('h = ' + str(bbox_c[3]))
 	print(center_c_x)
 	print(center_c_y)
-
+	
+	#~ calculate difference between center points
+	#~ ppm - pixels per meter, width of car divided by 2
+	#~ distance in meters is equal distance in pixels diveded by ppm
 	distance_pixels = m.sqrt(m.pow(float(center_c_x - center_p_x), 2) + m.pow(float(center_c_y - center_p_y), 2))
 	ppm = ((bbox_c[2] + bbox_p[2]) / 2.0) / 2.0
 	distance_meters = distance_pixels / ppm
 	
+	#~ speed v = s[m]/t[s]
 	speed = distance_meters / seconds
 	
+	#~ print ppm, time, s[pixels], s[meters], velocity
+	#~ return calculated speed
 	print('ppm = ' + str(ppm))
 	print('t = ' + str(seconds) + ' s')
 	print('s = ' + str(distance_pixels) + ' pixel')
@@ -48,53 +64,68 @@ def estimate_speed(bbox_p, bbox_c, seconds):
 	print('v = ' + str("%.2f" % round(speed, 2)) + ' m/s\n')
 	return speed
 
+#---------------------------------------------------------------------
+# Detection and tracking
+#---------------------------------------------------------------------
 def tracker():
 	rectangleColor = (0, 0, 255)
 	frameCounter = 0
 	currentCarID = 0
 	
+	#~ dictionary for locations
 	previous_location = {}
 	current_location = {}
 	
+	#~ dictionary for trackers
 	carTracker = {}
-	v = []
-	median = []
-	
+
 	while True:
+		#~ read frame and check it, if it is not frame break
 		rc, image = video.read()
 		if type(image) == type(None):
 			break
 		
+		#~ start time of iteration
+		#~ crop frame
+		#~ copy cropped frame
+		#~ add 1 to frame counter
 		start = time.time()
 		image = image[150:720, 0:950]
 		resultImage = image.copy()
-		
 		frameCounter = frameCounter + 1
 		
+		#~ create empty list for trackers you need to delete
 		carIDtoDelete = []
 		
+		#~ iterate trough dictionary of created trackers and if object leave frame delete tracker
 		for carID in carTracker.keys():
+			#~ get position of bounding box
 			trackedPosition = carTracker[carID].update(image)
-			#print(trackedPosition)
 			t_x, t_y, t_w, t_h = trackedPosition[1]
 			t_x = int(t_x)
 			t_y = int(t_y)
 			t_w = int(t_w)
 			t_h = int(t_h)
 			
-			x_center = t_x + 0.5 * t_w
-			y_center = t_y + 0.5 * t_h
+			#~ x_center = t_x + 0.5 * t_w
+			#~ y_center = t_y + 0.5 * t_h
 			
+			#~ if object leave frame add tracker to delete list
 			if t_x + t_w >= 750:
 				carIDtoDelete.append(carID)
-				
+		
+		#~ delete all trackers in delete list		
 		for carID in carIDtoDelete:
 			print('Tracker deleted: ' + str(carID) + '.')
 			carTracker.pop(carID, None)
 		
+		#~ try to detect new object in frame in every 10 frames
 		if not (frameCounter % 10):
+			#~ convert frame to grayscale
+			#~ try to detect new object in frame 
 			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 			cars = carCascade.detectMultiScale(gray, 1.1, 13)
+			#~ if object is detected, save it location and calculate center point of bounding box
 			for (_x, _y, _w, _h) in cars:
 				x = int(_x) + 5
 				y = int(_y) + 5
@@ -104,10 +135,11 @@ def tracker():
 				x_bar = x + 0.5 * w
 				y_bar = y + 0.5 * h
 				
-				
 				matchCarID = None
-			
+				
+				#~ iterate trough tracked objects
 				for carID in carTracker.keys():
+					#~ get object location and calculate center point of tracked object
 					trackedPosition = carTracker[carID].update(image)
 					t_x, t_y, t_w, t_h = trackedPosition[1]
 					t_x = int(t_x)
@@ -117,22 +149,25 @@ def tracker():
 
 					t_x_bar = t_x + 0.5 * t_w
 					t_y_bar = t_y + 0.5 * t_h
-										
+					
+					#~ if condition is true, detected object already have tracker 
 					if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
 						matchCarID = carID
-								
+				
+				#~ if detected object don't have tracker yet, create new tracker and add it to tracker dictionary
+				#~ save object location for speed estimation
 				if matchCarID is None:
 					bbox = (x, y, w, h)
 					if bbox[0] < 500 or bbox[1] < 100:
-						#tracker = cv2.Tracker_create('BOOSTING') #tracker
-						#tracker = cv2.TrackerBoosting_create() #tracker
-						tracker = cv2.TrackerMedianFlow_create() #tracker
+						tracker = cv2.TrackerMedianFlow_create()
 						tracker.init(image, bbox)
-						carTracker[currentCarID] = tracker	
+						carTracker[currentCarID] = tracker
 						previous_location[currentCarID] = bbox
 						currentCarID = currentCarID + 1
-					
+		
+		#~ in every frame iterate trough trackers
 		for carID in carTracker.keys():
+			#~ get position of object
 			trackedPosition = carTracker[carID].update(image)
 			t_x, t_y, t_w, t_h = trackedPosition[1]
 			t_x = int(t_x)
@@ -143,6 +178,8 @@ def tracker():
 			t_x_bar = t_x + 0.5 * t_w
 			t_y_bar = t_y + 0.5 * t_h
 			bbox = (t_x, t_y, t_w, t_h)
+			
+			#~ if detected object don't have tracker yet
 			corners = cv2.goodFeaturesToTrack(gray, 25, 0.01, 10)
 			if type(corners) != type(None):
 				corners = np.int0(corners)
@@ -151,29 +188,37 @@ def tracker():
 				if t_x < x < t_x + t_w and t_y < y < t_y + t_h:
 					cv2.circle(resultImage, (x, y), 5, 255, -1)
 			
+			#~ save location for speed estimation
+			#~ draw new rectangle in frame 
 			current_location[carID] = bbox
-				
 			cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 2)
 		
-		
+		#~ end time of iteration
+		#~ calculate time in seconds
+		#~ calculate frame per seconds (fps)
+		#~ put fps on frame
 		end = time.time()
 		seconds = end - start
 		fps = 1.0 / seconds
 		cv2.putText(resultImage, 'FPS: ' + 	str(int(fps)), (800, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 3)
 		
+		#~ iterate trough locations
 		for i in previous_location.keys():
-			if frameCounter % 1 == 0:
-				bbox_p = previous_location[i]
-				bbox_c = current_location[i]
-				
-				previous_location[i] = current_location[i]
-				if bbox_p != bbox_c:
-					estimate_speed(bbox_p, bbox_c, seconds)
+			#~ save location to local variables
+			#~ current location is new previous location
+			#~ if coordinates of location is different estimate speed
+			bbox_p = previous_location[i]
+			bbox_c = current_location[i]
+			previous_location[i] = current_location[i]
+			if bbox_p != bbox_c:
+				estimate_speed(bbox_p, bbox_c, seconds)
 		
+		#~ show results
+		#~ wait for esc to terminate
 		cv2.imshow('image', resultImage)
 		if cv2.waitKey(33) == 27:
 			break
-			
+	#~ close all open
 	cv2.destroyAllWindows()
 if __name__ == '__main__':
 	tracker()
